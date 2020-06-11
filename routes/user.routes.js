528968f588a2a171b1b5bba4454f6dfd6c8234df
Toni <sancheztoni.garcia@gmail.com>
2020-06-11 22:30:20 +0200

    users, routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,11 @@
-const { Router } = require('express');
-const User = require('../models/User.model');
+const { Router } = require("express");
+const User = require("../models/User.model");
 
-router.get('/user-profile', (req, res, next) => {
-    const {username, companies} = req.session.currentUser;
-    res.render('user/user-profile', {username, companies})
-})
+const router = Router();
+
+router.get("/user-profile", (req, res, next) => {
+  const { username, companies } = req.session.currentUser;
+  res.render("user/user-profile", { username, companies });
+});
+
+module.exports = router;
